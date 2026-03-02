65ff186121a711945b5fab9f9fd62d66b9d3d214
Raehan Ramadhan <muhamadraehan949@gmail.com>
2026-03-02 20:41:24 +0700

    UDAH AMAN SEMUA ANJAYYY

--- src/middleware.ts
+++ src/middleware.ts
@@ -458,7 +458,7 @@ function buildCSP(nonce: string): string {
     if (!IS_PROD) {
         return [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com ws://localhost:* http://localhost:*",
+            `script-src 'self' 'nonce-${nonce}' 'unsafe-inline' https://challenges.cloudflare.com ws://localhost:* http://localhost:*`,
             "script-src-attr 'unsafe-inline'",
             "style-src 'self' https://fonts.googleapis.com 'unsafe-inline'",
             "style-src-attr 'unsafe-inline'",
@@ -474,13 +474,12 @@ function buildCSP(nonce: string): string {
         ].join("; ");
     }
 
-    // BAGIAN PRODUCTION
     return [
         "default-src 'self'",
         `script-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'wasm-unsafe-eval' https://challenges.cloudflare.com`,
-        "script-src-attr 'unsafe-inline'", // Mengizinkan onerror, onclick, dll
-        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com", // Hapus nonce di sini agar style="..." jalan
-        "style-src-attr 'unsafe-inline'", // Mengizinkan atribut style di HTML
+        "script-src-attr 'unsafe-inline'",
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+        "style-src-attr 'unsafe-inline'",
         "font-src 'self' https://fonts.gstatic.com data:",
         "frame-src https://challenges.cloudflare.com",
         `connect-src 'self' ${supabaseOrigin} wss://*.supabase.co https://challenges.cloudflare.com`,
